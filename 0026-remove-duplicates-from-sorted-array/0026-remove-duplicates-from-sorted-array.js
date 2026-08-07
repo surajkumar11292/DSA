2f735de3c80dc54a8a2c50bf4f40cf6d50f4844f
Suraj Kumar <surajkumar11292@gmail.com>
2026-08-07 17:39:52 +0530

    Time: 1 ms (48.85%), Space: 58.6 MB (15.8%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let unique = 1;
+    let i =0;
+    let j=1;
+    let n = nums.length
+
+    while(j<n)
+    {
+
+        if(nums[i]==nums[j]){
+            j++;
+
+        }
+        else{
+            nums[i+1]=nums[j];
+            unique++;
+            j++;
+            i++;
+        }
+
+    }
+    return unique;
+
+
+};
